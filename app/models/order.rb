d035e0f0396ec02daf33d0837cd64171d835cf59
Umair Ahmed <umairahmed200203.umair@gmail.com>
2024-03-19 14:57:14 +0530

    stock validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,18 +10,6 @@ class Order < ApplicationRecord
 
   has_many :line_items, dependent: :destroy
 
-  def indian_states
-    [
-      "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
-      "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka",
-      "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram",
-      "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana",
-      "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal", "Andaman and Nicobar Islands",
-      "Chandigarh", "Dadra and Nagar Haveli and Daman and Diu", "Delhi", "Lakshadweep", "Puducherry"
-    ]
-  end
-
-
 
   def order_total
     tot = 0
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,6 +9,7 @@ class Stock < ApplicationRecord
   validates :size, presence: true
   validates :piece, presence: true
   validate :unique_sizes_per_color
+  validate :valid_size_format
 
   def available?
     self.piece > 0
@@ -17,6 +18,12 @@ class Stock < ApplicationRecord
   def unique_sizes_per_color
     if Stock.where(color_id: color_id, size: size).where.not(id: id).exists?
       errors.add(:size, "#{self.size} already exists")
+    end
+  end
+
+  def valid_size_format
+    unless size =~ /\A(XS|S|M|L|XL|XXL|XXXL)\z/
+      errors.add(:size, "is not valid. It should be one of XS, S, M, L, XL, XXL, or XXXL")
     end
   end
 end
